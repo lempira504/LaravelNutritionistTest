1529b8a4340be4497e700084f288e314ba870bc7
Heber Pavon <heberpavon@Hebers-iMac.local>
2020-03-09 22:57:43 -0400

    Created a new branch RemoteBranch and modified appProviders

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
 
 use Carbon\Carbon;
 
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Builder::defaultStringLength(191);
     }
 }
